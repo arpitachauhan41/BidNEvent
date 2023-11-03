da61ee2964567887f159120d56d78d7f3c17671a
arpitachauhan41 <137207545+arpitachauhan41@users.noreply.github.com>
2023-11-03 22:32:10 +0530

    Add files via upload

---
+++ models/Vendor.js
@@ -0,0 +1,25 @@
+//signup_vendor
+const mongoose=require('mongoose');
+const {Schema} = mongoose;
+
+const vendorSchema = new Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    phone_num:{
+        type:Number,
+        require:true
+    }
+})
+const Vendor = mongoose.model('Vendor',vendorSchema);
+module.exports = Vendor;
