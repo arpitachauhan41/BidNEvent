04cf5460878392a177af1cc5b29a3190cbd230e3
arpitachauhan41 <137207545+arpitachauhan41@users.noreply.github.com>
2023-11-04 17:09:29 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongourl = "mongodb://localhost:27017/?directConnection=true";
+const mongourl = "mongodb://127.0.0.1:27017/?directConnection=true&tls=false";
 const connectToMongo = ()=>{
     mongoose.connect(mongourl)
     .then(()=>{console.log("DB CONNECTED")})
